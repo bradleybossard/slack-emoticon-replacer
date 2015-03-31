eeb9096cf2743c5242fe5f8b26653c49cc446e89
Bradley Bossard <bradleybossard@gmail.com>
2015-03-31 14:02:15 -0700

    Added button to toggle feature on and off, and .css for button placement

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,11 +3,24 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
+    var isEmotifyOn = false;
+
     // Watch the input for changes.
     $('#message-input').bind('input propertychange', function() {
       console.log(this.value);
     });
 
+    //$('<a id="emotify_toggle" title="Toggle Emotification" class="flexpane_toggle_button"><i class="ts_icon ts_icon_mentions"></i></a>')
+    $('<button id="emotify_toggle">Emoticons Off</button>')
+      .on('click', function() {
+        if (isEmotifyOn) {
+          $(this).text('Emoticons Off');
+        } else {
+          $(this).text('Emoticons On');
+        }
+        isEmotifyOn = !isEmotifyOn;
+      })
+      .insertBefore('#channel_members_toggle');
 	}
 	}, 10);
 });
